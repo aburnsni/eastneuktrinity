8096f11f7dc97ef3d4c82f9f45aa358b7f6b8e5c
Andrew Burns <aburnsni@gmail.com>
2018-07-24 16:45:23 +0100

    Fixed votd to use JSON and background image

---
+++ js/votd.js
@@ -0,0 +1,14 @@
+$.getJSON('https://cors.io/?https://www.biblegateway.com/votd/get/?format=json', function(data) {
+    
+    var text = `<div class=\"lead votd-text\">${data.votd.text}</div>
+                <div class=\"votd-ref\"><a href=\"${data.votd.permalink}\">${data.votd.display_ref}</a>
+                <a rel=\"nofollow\" href=\"${data.votd.audiolink}\" title=\"Listen to chapter\"><img alt=\"Listen to chapter\" src=\"//www.biblegateway.com/assets/images/audio/sound.gif\" border=\"0\"></a>
+                <a href=\"${data.votd.copyrightlink}\">${data.votd.copyright}</a>
+                </div>
+                <div class=\"small\">
+                  Provided by 
+                    <a rel=\"nofollow\" href=\"https://www.biblegateway.com/\">BibleGateway.com</a>
+                </div>`
+    
+    $(".verse").html(text);
+});       
