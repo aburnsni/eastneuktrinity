4d52b3f04ce2f38fceeefaa67d36b287758aae1e
Andrew Burns <aburnsni@gmail.com>
2018-07-26 13:59:14 +0100

    Tweaked verse layout

--- js/votd.js
+++ js/votd.js
@@ -1,8 +1,10 @@
 $.getJSON('/js/verse.json', function(data) {
     
-    var text = `<div class=\"lead votd-text\">${data.votd.text}</div>
-                <div class=\"votd-ref\"><a href=\"${data.votd.permalink}\">${data.votd.display_ref}</a>
+    var text = `<div class=\"lead votd-text\">${data.votd.text}<br />
+                <span class=\"votd-ref\"><a href=\"${data.votd.permalink}\">${data.votd.display_ref}</a>
                 <a rel=\"nofollow\" href=\"${data.votd.audiolink}\" title=\"Listen to chapter\"><img alt=\"Listen to chapter\" src=\"//www.biblegateway.com/assets/images/audio/sound.gif\" border=\"0\"></a>
+                </span></div>
+                </div class="votd-copy">
                 <a href=\"${data.votd.copyrightlink}\">${data.votd.copyright}</a>
                 </div>
                 <div class=\"small\">
